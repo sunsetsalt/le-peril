0a712d7ca81ca98a45f7a1e9f862bda561bf216c
Miid <mid@doe.com>
2025-08-15 23:16:25 +0200

    fix: router basename + index route + links for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/le-peril/',  // <-- 🔴 change this to your repo name
+  base: '/le-peril/',  // <-- exact repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
